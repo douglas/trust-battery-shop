c0e09cef7e36fbc09f9c232434d44debf9e9dfc0
Swati Swoboda <swati.swoboda@shopify.com>
2019-12-12 09:26:33 -0500

    Switch to 0-4 model instead of 0-5.

--- constants.js
+++ constants.js
@@ -25,7 +25,7 @@ export type MilestoneMap = {
   'RECRUITING': Milestone,
   'COMMUNITY': Milestone
 }
-export const milestones = [0, 1, 2, 3, 4, 5]
+export const milestones = [0, 1, 2, 3, 4]
 
 export const milestoneToPoints = (milestone: Milestone): number => {
   switch (milestone) {
@@ -34,7 +34,6 @@ export const milestoneToPoints = (milestone: Milestone): number => {
     case 2: return 3
     case 3: return 6
     case 4: return 12
-    case 5: return 20
     default: return 0
   }
 }
