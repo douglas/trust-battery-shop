f7205041f5910ae2b5c2c7d154465ecc73d1c416
Swati Swoboda <swati.swoboda@shopify.com>
2019-12-13 11:44:40 -0500

    add graphql

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bc5c9a64ce33d2a8130c";
+/******/ 	var hotCurrentHash = "3e34a4f7a91725d06980";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
