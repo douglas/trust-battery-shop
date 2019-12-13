e1af4bb39457a0b7867757f96b3ed937ecbbcb95
Swati Swoboda <swati.swoboda@shopify.com>
2019-12-13 13:58:45 -0500

    auto-bump package

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
+import packageJson from '../package.json'
+
 export default () => (
   <div style={{display: 'flex', paddingBottom: '20px'}}>
     <div style={{flex: 1}}>
-      Based on the <a href="https://github.com/Medium/snowflake" target="_blank">Snowflake Model</a> by Medium. Version 1.2.
+      Based on the <a href="https://github.com/Medium/snowflake" target="_blank">Snowflake Model</a> by Medium. Version {packageJson.version}.
     </div>
   </div>
 )
