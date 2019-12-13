39cfdd8639b1f86f42884b821cfd2a5daad1af02
Swati Swoboda <swati.swoboda@shopify.com>
2019-12-13 11:44:59 -0500

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3e34a4f7a91725d06980";
+/******/ 	var hotCurrentHash = "082e7407900ec52e4498";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
