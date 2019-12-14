051cb1a9c55804dffdc1e7857665596f4091667e
Swati Swoboda <swati.swoboda@shopify.com>
2019-12-14 15:55:01 -0500

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a99fd5f64acdacfa1b18";
+/******/ 	var hotCurrentHash = "f8de896f5a7ea9be6f0d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.a99fd5f64acdacfa1b18.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./package.json":
+/*!**********************!*\
+  !*** ./package.json ***!
+  \**********************/
+/*! exports provided: name, version, main, repository, homepage, license, dependencies, scripts, devDependencies, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"name\":\"trust-battery\",\"version\":\"0.1.0\",\"main\":\"index.js\",\"repository\":\"https://github.com/sjswoboda/trust-battery\",\"homepage\":\"https://sjswoboda.github.io/trust-battery/\",\"license\":\"MIT\",\"dependencies\":{\"@apollo/react-hooks\":\"^3.1.3\",\"apollo-boost\":\"^0.4.4\",\"d3\":\"^4.11.0\",\"gh-pages\":\"^2.1.1\",\"graphql\":\"^14.5.8\",\"next\":\"^9.0.7\",\"react\":\"^16.10.2\",\"react-dom\":\"^16.10.2\"},\"scripts\":{\"dev\":\"next\",\"jekyll\":\"cp .nojekyll ./out\",\"predeploy\":\"next build && next export && npm run jekyll\",\"deploy\":\"gh-pages -d out -t\",\"flow\":\"flow\"},\"devDependencies\":{\"@babel/preset-flow\":\"^7.0.0\",\"flow-bin\":\"^0.57.2\"}}");
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.a99fd5f64acdacfa1b18.hot-update.js.map
